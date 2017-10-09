872e20481167329fab89403bb4548b017a93caeb
Qazeke <sergiozgz18@gmail.com>
2017-10-09 15:20:53 +0200

    Implementado numero de triangulos

--- Basicos/triangulos.py
+++ Basicos/triangulos.py
@@ -1,9 +1,11 @@
 #! /usr/bin/python3
 
-def triangulo(lado=10, caracter='*'):
-    for i in range (0, lado + 1):
-        print (caracter * i)
-    for i in range (lado, 0, -1):
-        print(caracter * i)
+def triangulo(numero=1,lado=10, caracter='*'):
+    for j in range (0, numero):
+        for i in range (0, lado + 1):
+            print (caracter * i)
+        for i in range (lado, 0, -1):
+            print(caracter * i)
 
-triangulo()
+
+triangulo(lado=5)
